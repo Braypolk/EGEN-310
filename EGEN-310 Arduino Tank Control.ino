e24901d29ae7b6067071705dd1951f5d8b83c01a
Braypolk <braypolk@gmail.com>
2019-12-05 18:25:18 -0700

    Update EGEN-310 Arduino Tank Control.ino

--- EGEN-310 Arduino Tank Control.ino
+++ EGEN-310 Arduino Tank Control.ino
@@ -10,12 +10,15 @@ Adafruit_DCMotor *leftMotor = AFMS.getMotor(4);
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(3);
 Adafruit_DCMotor *pivotMotor = AFMS.getMotor(2);
 
+//Initialize Variables
 String inString = "";    // string to hold input
 String left, leftDir, right, rightDir, pivot, pivotDir;
 
 void setup() {
+  //start serial communication
     Serial.begin(9600);
     Serial.println("running");
+    //turn on motors
     AFMS.begin();
     leftMotor->setSpeed(150);
     rightMotor->setSpeed(150);
@@ -32,25 +35,32 @@ void setup() {
 
 void loop() {
 
+  //if serial data is recieved, procede with code
   while(Serial.available() == 0){}
+
+  //get data being sent from processing and store as string
   inString = Serial.readStringUntil('\n');
   int i, j, k;
-  
+
+  //store speed and direction of motor 1
   for(i = 0; inString.charAt(i) != ','; i++){}
   
   left = inString.substring(0, i);
   leftDir = inString.substring(i+1, i+2);
-  
+
+  //store speed and direction of motor 2
   for(j = i+3; inString.charAt(j) != ','; j++){}
   
   right = inString.substring(i+3, j);
   rightDir = inString.substring(j+1, j+2);
-  
+
+  //store speed and direction of motor 3
   for(k = j+3; inString.charAt(k) != ','; k++){}
   
   pivot = inString.substring(j+3, k);
   pivotDir = inString.substring(k+1, inString.length());
-  
+
+  //set motor direction based on value stored above
   if(leftDir.toInt() == 1){
     leftMotor->run(FORWARD);
   }
@@ -70,8 +80,12 @@ void loop() {
   else{
     pivotMotor->run(FORWARD);
   }
-leftMotor->setSpeed((unsigned int)left.toInt());
-rightMotor->setSpeed((unsigned int)right.toInt());
-pivotMotor->setSpeed((unsigned int)pivot.toInt());
-delay(2);
+
+  //set motor speed
+  leftMotor->setSpeed((unsigned int)left.toInt());
+  rightMotor->setSpeed((unsigned int)right.toInt());
+  pivotMotor->setSpeed((unsigned int)pivot.toInt());
+
+  //delay just enough to not have backup in buffer
+  delay(2);
 }
